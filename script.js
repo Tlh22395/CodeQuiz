e7a96fc1fbfa978838e8b9677be675a9261f57a9
Tareeq Hamilton <tareeqhamilton@gmail.com>
2020-12-14 20:55:05 -0500

    added functions for start button and timer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//created variables for buttons, answers, and quiz
 var startButton = document.getElementById("start");
 var startScreen = document.getElementById("startScreen");
 var questionsScreen = document.getElementById("questionsScreen");
@@ -5,33 +6,92 @@ var endScreen = document.getElementById("endScreen");
 var answers = [] 
 var index = 0
 var score = 0
-var secondsLeft = 40
+var seconds = 40
 var timer = document.getElementById("time")
 
+//questions for quiz
 var questions = [
     {
     q: "In pro basketball, how high is the basketball hoop from the ground?",
     answers: ["12", "10", "8", "6"],
-    CorrectAnswer: "12"
+    correctAnswer: "12"
     },
     {
     q: "What sport is deemed 'King of Sports'?",
     answers: ["Football", "Soccer", "Baseball", "Wrestling"],
-    CorrectAnswer: "Soccer"
+    correctAnswer: "Soccer"
     },
     {
     q: "In football, how many points is a touchdown worth?",
     answers: ["6", "3", "2", "9"],
-    CorrectAnswer: "6"
+    correctAnswer: "6"
     },
     {
     q: "What is Canada's National Sport?",
     answers: ["Hockey", "Football", "Lacrosse", "Basketball"],
-    CorrectAnswer: "Lacrosse"
+    correctAnswer: "Lacrosse"
     },
     {
     q: "During the first ever modern Olympics, what were the first placers awarded with?",
     answers: ["Bronze Medals", "Ribbons", "Gold Medals", "Silver Medals"],
-    CorrectAnswer: "Silver Medals"
+    correctAnswer: "Silver Medals"
     },
 ];
+// function for quiz questions
+function questionMaker() {
+    if (index >= questions.length) {
+        endGame();
+        return;
+    }
+    console.log('questions', question[index])
+    questionsScreen.innerHTML = questions[index].q
+    for (var i = 0; i < question[index].answers.length; i++) {
+        var answerBtn = document.createElement("Button");
+        answerBtn.innerHTML = questions[index].answers[i];
+        answerBtn.setAttribute("value", question[index].answers[i]);
+        questionsScreen.appendChild(answerBtn);
+
+        answerBtn.addEventListener("click", function (quiz) {
+            console.log(quiz.target.value)
+            if(quiz.target.value === questions[index].correctAnswer) {
+                score++
+                console.log("Correct Answers")
+                console.log("Score")
+            }
+            else if (quiz.target.value !== question[index].correctAnswer) {
+                (secondsLeft = - 5)
+                console.log("incorrect")
+            }
+            index++;
+            questionMaker();
+        })
+    }
+}
+
+//function for start button
+startButton.addEventListener("click", function () {
+    console.log("button clicked")
+    startScreen.style.display = "none";
+    questionsScreen.display.innerHTML = "questionsScreen";
+
+    setTimer();
+    questionMaker();
+})
+
+//create function for timer
+function setTimer(){
+    var timeInt = setInterval (function(){
+        seconds--;
+        timer.textContent = seconds === 0;
+            clearInterval(timeInt)
+
+    }, 1000)
+
+}
+function endGame() {
+    console.log("Thank you for playing!!")
+    questionsScreen.style.display = "none";
+    endScreen.innerHTML = "Your Score: " + score + questions.length
+};
+
+
